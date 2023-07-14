e5498cf0ef252d5e6458ba9d8d2eccc1335868ad
User <User@TERARCON>
2023-07-14 19:36:49 +0800

    Commited

--- String/Source.cpp
+++ String/Source.cpp
@@ -1,19 +1,29 @@
 #include<iostream>
+
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
-#define tab "\t"
-#define delimiter "\n-------------------------------------------\n"
+#define tad "\t"
+#define delimiter "\n-------------------------------------------------------------------------\n"
+//#define CONSTRUCTORS
 
+class String;
+ostream& operator<<(ostream& os, const String& obj);
+String operator+(const String& left, const String& right);
+
+//????? string ???????????? ??? ?????? ?? ???????? ???? char*, ??????? ???????????? ????? ?????? ? ??????????? ?????. 
 class String
 {
-	int size;	
-	char* str;	
+	char* str; // ????? ??????(?????????) ? ???????????? ??????
+	int size; // ?????? ?????? ? ??????
 public:
 	int get_size()const
 	{
 		return size;
 	}
-	const char* get_str()const
+	const char* get_str() const
 	{
 		return str;
 	}
@@ -22,49 +32,59 @@ public:
 		return str;
 	}
 
-	//				Constructors:
-	explicit String(int size = 80)
+	//                                            Constructors:	
+	explicit String(int size = 80)// ??????????? ?????????
 	{
 		this->size = size;
 		this->str = new char[size] {};
 		cout << "DefConstructor:\t" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str) // ??????????? //char* - ?????????
 	{
-		this->size = strlen(str) + 1;
+		this->size = strlen(str) + 1; // strlen - ??????? ????? ?????? //? ?????? ?????????????? ????
 		this->str = new char[size] {};
-		for (int i = 0; str[i]; i++)this->str[i] = str[i];
+		for (int i = 0; i < size; i++) // ??????????? ? ???
+		{
+			this->str[i] = str[i];
+		}
 		cout << "Constructor:\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other) // ??????????? ??????????? ????????
 	{
 		//Deep copy
 		this->size = other.size;
 		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		for (int i = 0; i < size; i++) // ??????????? ? ???
+		{
+			this->str[i] = other.str[i];
+		}
 		cout << "CopyConstructor:\t" << this << endl;
 	}
-	~String()
+	String(String&& other) // MoveConstructor - ??????????? ????????
+	{
+		// https://legacy.cplusplus.com/doc/tutorial/classes2/
+		//??????????? ??????????? ??????????, ????? ?????? ???????????????? ??? ?????????? ? ?????????????? ???????????? ?????????? ???????. 
+		//??????????, ?????????? ??????????? ??????????, ????? ??????? ????????????? ???????? ???????????? ??????????
+		this->size = other.size;
+		this->str = other.str;
+		other.str = nullptr;
+		cout << "MoveConstructor:\t" << this << endl;
+
+	}
+	~String() // ????????
 	{
 		delete[] str;
 		cout << "Destructor:\t" << this << endl;
 	}
-
-	//						Operators:
-	String& operator=(const String& other)
+	//                                                Methods:
+	void print()const
 	{
-		if (this == &other)return *this;
-		int a = 2;
-		int b = 3;
-		a = b;
-		delete[] this->str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
+		cout << "size: \t" << size << endl;
+		cout << "str: \t" << str << endl;
 	}
-	char operator[](int i)const
+	//                                                Onerators:
+
+	char operator[](int i) const
 	{
 		return str[i];
 	}
@@ -72,52 +92,93 @@ public:
 	{
 		return str[i];
 	}
-
-	//						Methods:
-	void print()const
+	String& operator=(const String& other)
 	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
+		if (this == &other) return *this;
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		cout << "CopyConstructor" << this << endl;
+		return *this;
 	}
+	String& operator=(String&& other) //Move-assignment
+	{
+		delete[] this->str;
+		this->size = other.size;
+		this->str = other.str;
+		other.str = nullptr;
+		cout << "MoveAssignment" << this << endl;
+		return *this;
+	}
+
+
 };
 
-String operator+(const String& left, const String& right)
-{
-	String cat(left.get_size() + right.get_size() - 1);
-	for (int i = 0; i < left.get_size(); i++)
-		cat[i] = left[i];
-	//cat.get_str()[i] = left.get_str()[i];
-	for (int i = 0; i < right.get_size(); i++)
-		cat[i + left.get_size() - 1] = right[i];
-	//cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
-	return cat;
-}
-
-std::ostream& operator<<(std::ostream& os, const String& obj)
+//                                                Onerators:
+ostream& operator<<(ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
+String operator+(const String& left, const String& right)
+{
+	/*const int a = 2;
+	a = 3;*/
+	String res(left.get_size() + right.get_size() - 1);
+	for (int i = 0; i < left.get_size(); i++)
+	{
+		res[i] = left[i];
+		//res.get_str()[i] = left.get_str()[i];
+	}
+	for (int i = 0; i < right.get_size(); i++)
+	{
+		res[i + left.get_size() - 1] = right[i];
+		//res.get_str()[i+left.get_size() - 1] = left.get_str()[i];
+	}
+	return res;
+}
+
+
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef CONSTRUCTORS
 	cout << sizeof("Hello") << endl;
-	String str(5);
+	String str;
 	str.print();
-
-	String str1 = "Hello";
-	str1 = str1;
-	cout << str1 << endl;
-
-	String str2 = "World";
-	cout << str2 << endl;
-
 	cout << delimiter << endl;
-	String str3 = str1 + str2;
+
+	String str2(5);
+	str2.print();
+	cout << delimiter << endl;
+
+	String str3 = "Hello";
+	str3 = str3;
 	cout << str3 << endl;
 	cout << delimiter << endl;
 
-	String str4;
-	str4 = str1 + str2;
+	String str4 = "World";
 	cout << str4 << endl;
+
+	cout << delimiter << endl;
+	String str5 = str3 + " " + str4;
+	cout << str5 << endl;
+
+	cout << delimiter << endl;
+	String str6;
+	str6 = str3 + str4;
+	cout << str6 << endl;
+#endif // CONSTRUCTORS
+	String str1("Hello");
+	String str2("World");
+	cout << delimiter << endl;
+	String str3 = str1 + str2;
+	//str3 = str1 + str2;
+	cout << delimiter << endl;
+	cout << "str 3 = " << str3 << endl;
+
 }
